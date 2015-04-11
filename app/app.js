e31ba17eb3cacf72d5323a9299ef8dd1030d121c
Rogelio Alberto <i@rog.mx>
2015-04-11 00:22:33 -0500

    Sticky header and use ng-src to fix src error in inspector

--- app/app.js
+++ app/app.js
@@ -12,4 +12,29 @@ angular.module('Cvitae', [])
       	});
   	}]);
 
-
+var lastScrollTop = 0; 
+document.addEventListener('scroll', function (event) {
+	var sticky =  document.getElementById('me');
+	var cv =  document.getElementById('cv');
+	var window_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	var st = window.pageYOffset, direction = 0;
+	var offset_height = sticky.offsetHeight;
+	if( window_width > 500 ){
+		if (st > lastScrollTop) {
+			direction = 0;
+		} else if (st < lastScrollTop ){
+			direction = 1;
+		}
+		if ( !direction ){
+			offset_height = offset_height - 120;
+		}
+		if( document.body.scrollTop > offset_height ){
+			sticky.className = 'sticky';
+			cv.style.paddingTop = '290px';
+		} else {
+			sticky.className = '';
+			cv.style.paddingTop = 0;
+		}
+		lastScrollTop = st;
+	}
+});
